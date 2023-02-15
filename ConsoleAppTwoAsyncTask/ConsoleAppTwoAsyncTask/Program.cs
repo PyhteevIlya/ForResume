e86b7a5ee6d093c453394f5e20f6096faa3d0207
PyhteevIlya <112899541+PyhteevIlya@users.noreply.github.com>
2023-02-15 11:49:25 +0500

    NewFile

---
+++ ConsoleAppTwoAsyncTask/ConsoleAppTwoAsyncTask/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var task1 = FirstTaskAsync();
+        var task2 = SecondTaskAsync();
+
+        await Task.WhenAll(task1, task2);
+
+        Console.WriteLine($"first task result: {task1.Result}; second task result: {task2.Result}");
+
+        async Task<string> FirstTaskAsync()
+        {
+            int i = 0;
+            string s = "";
+            while (i != 10)
+            {
+                i++;
+                Console.WriteLine(i);
+                await Task.Delay(new Random().Next(500, 1000));
+                s += i.ToString()+" ";
+            }
+            return s;
+        }
+
+        async Task<string> SecondTaskAsync()
+        {
+            int i = 0;
+            string s = "";
+            while (i != 100)
+            {
+                i += 10;
+                Console.WriteLine(i);
+                await Task.Delay(new Random().Next(500, 1000));
+                s += i.ToString()+" ";
+            }
+            return s;
+        }
+    }
+}
---
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+            // добавление элементов
+            linkedList.Add("Bob");
+            linkedList.Add("Bill");
+            linkedList.Add("Tom");
+            linkedList.Add("Frank");
+            linkedList.Add("Vlad");
+            linkedList.Add("Cute");
+            linkedList.AddFirst("Kate");
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+            // удаление
+            linkedList.Remove("Bill");
+            linkedList.Remove("Tom");
+            linkedList.RemoveFirst();
+            linkedList.RemoveEnd();
+            // перебор с последнего элемента
+            foreach (var t in linkedList.BackEnumerator())
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
