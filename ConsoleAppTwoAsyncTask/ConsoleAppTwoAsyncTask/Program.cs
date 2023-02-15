c21e4aedb52598f8a3a98ae75c0876f81d0d1617
PyhteevIlya <112899541+PyhteevIlya@users.noreply.github.com>
2023-02-15 16:20:30 +0500

    СhangesEx2

--- ConsoleAppTwoAsyncTask/ConsoleAppTwoAsyncTask/Program.cs
+++ ConsoleAppTwoAsyncTask/ConsoleAppTwoAsyncTask/Program.cs
@@ -1,40 +1,66 @@
-﻿internal class Program
+﻿using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Program
 {
-    private static async Task Main(string[] args)
+    static async Task Main(string[] args)
     {
-        var task1 = FirstTaskAsync();
-        var task2 = SecondTaskAsync();
+        Channel<string> s_channelFirst = Channel.CreateUnbounded<string>();
+        Channel<string> s_channelSecond = Channel.CreateUnbounded<string>();
+
+        ChannelReader<string> reader1 = s_channelFirst.Reader;
+
+        ChannelReader<string> reader2 = s_channelSecond.Reader;
+
+        var task1 = TaskFirst();
+        var task2 = TaskSecond();
 
         await Task.WhenAll(task1, task2);
 
-        Console.WriteLine($"first task result: {task1.Result}; second task result: {task2.Result}");
-
-        async Task<string> FirstTaskAsync()
+        static ChannelReader<T> Merge<T>(
+        ChannelReader<T> s_channelFirst, ChannelReader<T> s_channelSecond)
         {
-            int i = 0;
-            string s = "";
-            while (i != 10)
+            var output = Channel.CreateUnbounded<T>();
+
+            Task.Run(async () =>
             {
-                i++;
-                Console.WriteLine(i);
-                await Task.Delay(new Random().Next(500, 1000));
-                s += i.ToString()+" ";
-            }
-            return s;
+                await foreach (var item in s_channelFirst.ReadAllAsync())
+                    await output.Writer.WriteAsync(item);
+            });
+            Task.Run(async () =>
+            {
+                await foreach (var item in s_channelSecond.ReadAllAsync())
+                    await output.Writer.WriteAsync(item);
+            });
+            return output;
         }
 
-        async Task<string> SecondTaskAsync()
+        var ch = Merge(reader1, reader2);
+
+        await foreach (var item in ch.ReadAllAsync())
+            Console.WriteLine(item);
+
+        async Task TaskFirst()
         {
-            int i = 0;
-            string s = "";
-            while (i != 100)
+            ChannelWriter<string> writer1 = s_channelFirst.Writer;
+            for (int i = 1; i <= 10; i++)
             {
-                i += 10;
-                Console.WriteLine(i);
+                await s_channelFirst.Writer.WriteAsync(i.ToString());
                 await Task.Delay(new Random().Next(500, 1000));
-                s += i.ToString()+" ";
             }
-            return s;
+        }
+
+        async Task TaskSecond()
+        {
+            ChannelWriter<string> writer2 = s_channelSecond.Writer;
+            for (int i = 10; i <= 100; i += 10)
+            {
+                await s_channelSecond.Writer.WriteAsync(i.ToString());
+                await Task.Delay(new Random().Next(500, 1000));
+            }
         }
     }
 }
